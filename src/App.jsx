93236cfc51c6ad3e362ace7441a88c71b2dce70a
yumi-0223 <chym22048@g.nihon-u.ac.jp>
2024-01-10 13:00:31 +0900

    kiso

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+
+
+export default function App() {
+    
+    return (
+        <div>
+            <div>
+                <header>Apple Member</header>
+            </div>
+            <div>
+                <h1>
+                    <p>連絡事項</p>
+                </h1>
+                
+            </div>
+            <footer>日本大学文理学部情報科学科 Webプログラミングの演習課題　5422048　葛畑友美</footer>
+        </div>
+    )
+}
