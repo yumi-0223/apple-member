e6dbf7dec54514ac353debe292921fc84b69dce6
yumi-0223 <chym22048@g.nihon-u.ac.jp>
2024-01-10 17:04:23 +0900

    ue

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,16 @@ export default function App() {
                 <header>peach Member</header>
             </div>
             <div>
+                <h>
                 <h1>
                     <p>連絡事項</p>
                 </h1>
-                
+                </h>
             </div>
-            <footer>日本大学文理学部情報科学科 Webプログラミングの演習課題　5422048　葛畑友美</footer>
+            <footer className="footer">
+                <p>日本大学文理学部情報科学科 Webプログラミングの演習課題　5422048　葛畑友美</p>
+                <p>microCMSを利用</p>
+            </footer>
         </div>
     )
 }
